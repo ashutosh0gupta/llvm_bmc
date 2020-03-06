4aabed82004be5b1d71b3e616ab7d3c6c9140b7a
ASHUTOSH GUPTA <akg@iitb.ac.in>
2020-03-06 11:09:25 +0530

    wp files added

---
+++ src/lib/wp/wp.cpp
@@ -0,0 +1,5 @@
+#include <wp.h>
+
+expr wp( bb_vec_t , expr ) {
+  // implement here
+}
--- src/lib/wp/wp.h
+++ src/lib/wp/wp.h
@@ -1,4 +1,10 @@
 #ifndef WP_H
 #define WP_H
 
+#include<llvm_utils.h>
+#include<solver.h>
+
+
+expr wp( bb_vec_t , expr );
+
 #endif
