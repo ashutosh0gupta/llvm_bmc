f0a3eb402e3bddc972a596af816e4f0816a44959
PhongNgo <tuan-phong.ngo@it.uu.se>
2023-06-25 23:54:47 -0400

    Add some missing benchmarks

---
+++ examples/kbound/popl23-benchmarks/HMC/ARMCBMC/nwrites10/nwrites10.c
@@ -0,0 +1,35 @@
+#include <pthread.h>
+#include <stdatomic.h>
+#include <assert.h>
+
+# define N 10
+
+long x[N];
+long idx[N];
+
+void *thread_n(void *arg)
+{
+	int i = *((int *) arg);
+	atomic_store_explicit(&x[i], 42, memory_order_relaxed);
+	return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+	pthread_t t[N];
+
+	for (int i = 0; i < N; i++)
+                atomic_init(&idx[i], i);
+	for (int i = 0; i < N; i++) {
+		pthread_create(&t[i], NULL, thread_n, &idx[i]);
+	}
+	for (int i = 0; i < N; i++) {
+		pthread_join(t[i], NULL);
+	}
+	for (int i = 0; i < N; i++) {
+		assert(atomic_load_explicit(&x[i], memory_order_relaxed) == 42);
+	}
+
+	return 0;
+}
+
