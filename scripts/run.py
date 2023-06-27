6fb0effd36f4f2b8d02a6298fb43ec2434060f3e
Ashutosh Gupta <akg@iitb.ac.in>
2023-06-27 17:33:53 +0530

    sad

--- scripts/run.py
+++ scripts/run.py
@@ -21,8 +21,8 @@ l = 1
 
 # folder = "examples/kbound/omkar/bench"
 # folder = "./examples/kbound/popl23-benchmarks/TRACER/ARMCBMC/"
-# folder = "./examples/litmus/c/c-litmus-ARMCBMC/"
-folder = "./examples/kbound/popl23-benchmarks/HMC/ARMCBMC/"
+folder = "./examples/litmus/c/c-litmus-ARMCBMC/"
+# folder = "./examples/kbound/popl23-benchmarks/HMC/ARMCBMC/"
 
 
 # folder = "examples/kbound/pldi19-benchmark"
