92c7d40b9a4c28d44c8032bd4480c55a04129384
Digeshwar <digeshwar.dd786@gmail.com>
2025-06-09 01:56:19 +0530

    handled funcion pointer in read constat function

--- src/lib/utils/llvm_utils.cpp
+++ src/lib/utils/llvm_utils.cpp
@@ -1990,9 +1990,24 @@ expr read_const( options& o, const llvm::Value* op ) {
   }else if( llvm::isa<llvm::ConstantVector>(op) ) {
     // const llvm::VectorType* n = c->getType();
     llvm_bmc_error("llvm_utils", "vector constant not implemented!!" );
+  }else if (auto func = llvm::dyn_cast<llvm::Function>(op)) {
+    llvm::outs() << "Found function pointer: " << func->getName() << "\n";
+    
+    // Create a unique negative index for the function
+    static std::unordered_map<const llvm::Function*, int> func_indices;
+    static int next_func_idx = -2;  // Start at -2 (since -1 is used for null)
+    
+    // Get or create index for this function
+    if (!func_indices.count(func)) {
+      func_indices[func] = next_func_idx--;
+    }
+    
+    return ctx.int_val(func_indices[func]);
+  
   }else if( llvm::isa<llvm::Constant>(op) ) {
     // expr e(ctx);
     // return e; // contains no expression;
+    return get_fresh_bool(ctx);
     llvm_bmc_error("llvm_utils", "non int constants are not implemented!!" );
     std::cerr << "un recognized constant!";
     //     // int i = readInt(c);
